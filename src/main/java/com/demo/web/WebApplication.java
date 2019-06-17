f0962a87ed773a1cb6628a6dc87cb6a814ee2544
lgjlife <liang_gj17@163.com>
2019-06-17 15:35:58 +0800

    修改启动脚本

--- src/main/java/com/demo/web/WebApplication.java
+++ src/main/java/com/demo/web/WebApplication.java
@@ -9,5 +9,13 @@ public class WebApplication {
     public static void main(String args[]){
 
         SpringApplication.run(WebApplication.class,args);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+
+            @Override
+            public void run() {
+                System.out.println("do ShutdownHook.......... ");
+            }
+        });
     }
 }
